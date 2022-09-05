87d471a6a9ff42ac620a3dc30e3401bacc08ed70
Grigoriev Alexander <ag.1212@mail.ru>
2022-09-05 14:07:37 +0300

    sewcond

--- mod1.py
+++ mod1.py
@@ -2,3 +2,5 @@ def easyfunc(a,b):
     return a+b
 
 print (easyfunc(2,3))
+print (easyfunc(3,4))
+print (easyfunc(5,6))
--- mod2.py
+++ mod2.py
@@ -1,5 +1,5 @@
 from mod1 import easyfunc
-q=4
+q=14
 w=6
 e=easyfunc(q,w)
 print(e)
